ed26df4e31f0f2aa5f2eff8edf1ee6de1b9e95af
Cristian Saldua <crsaldua@student.apc.edu.ph>
2026-01-20 09:05:44 +0800

    HSHSHSHSHHS

--- firstsfc/src/components/personalprofile.vue
+++ firstsfc/src/components/personalprofile.vue
@@ -1,37 +1,62 @@
 <template>
-<div class="main-wrapper">
-<div class="grid-container">
-<div class="header">
-<h1>Personal Profile Web page </h1>
-<p>
-    <b>
-Im Cristian<br>
-Asia Pacific College<br>
-Course: BSIT- 242 <br>
-IT Experience: html and css<br>
-Hobbies & Interest: Playing online game with my friends<br>
-Goals In Life / Dream:to be rich <br>
-</b>
+  <div class="main-wrapper">
+    <div class="grid-container">
+      <div class="header">
+        <h1>Personal Profile Web page</h1>
+        <p>
+          <b>
+            Im Cristian<br>
+            Asia Pacific College<br>
+            Course: BSIT- 242<br>
+            IT Experience: html and css<br>
+            Hobbies & Interest: Playing online game with my friends<br>
+            Goals In Life / Dream: to be rich
+          </b>
+          <img 
+            src="https://media.licdn.com/dms/image/v2/D5603AQFpB9rCFiMUyw/profile-displayphoto-scale_200_200/B56ZqnKlsOKwAc-/0/1763741163274?e=2147483647&v=beta&t=vi0oROWu351SApec0r5CoHkB6NCjI2s3b4KE2pWIg-U" 
+            width="500" 
+            height="600"
+            alt="Cristian profile"
+          >
+        </p>
 
-<img src="https://media.licdn.com/dms/image/v2/D5603AQFpB9rCFiMUyw/profile-displayphoto-scale_200_200/B56ZqnKlsOKwAc-/0/1763741163274?e=2147483647&v=beta&t=vi0oROWu351SApec0r5CoHkB6NCjI2s3b4KE2pWIg-U"width="500" height="600">
-</p>
+        <h2>This is my friends And their Favorite thing</h2>
 
-<h2>This is my frinds And their Favorite thing</h2>
-
-<div class="container">
-  <div class = image><p>Dom's Favorite Hero The Milo King</p><img src="https://4.bp.blogspot.com/-BtuzkDDuVcU/WV9UpfARKNI/AAAAAAAAD0w/BbhY8uAL4KwGvIvDNO6uYrVGieFYVstLQCLcBGAs/s1600/milo%2Bman.jpg" alt="Milo King"></div>
-  <div class = image><p>Charles Favorite Movie Tangled</p><img src="https://i.pinimg.com/originals/b3/0a/ff/b30aff959dec31c10892e2c3df286fc1.png" alt="Tangled"></div>
-  <div class = image><p>Jared's Favorite Anime Character</p><img src="https://wallpapers.com/images/hd/katsuki-bakugo-my-hero-academia-iphone-hinkoepkef4nkqt2.jpg" alt="Jared's Bakugo"></div>  
-  <div class = image><p>This is my friend Dom</p><img src="https://tse3.mm.bing.net/th/id/OIP.KpRPWqy3d4BwI3xbagZoBQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Dom"></div>
-  <div class = image><p>This is my friend Charles</p><img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Charles"></div>
-  <div class = image><p>This is my friend Jared</p><img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="jared"></div>
-  
-</div>
-
+        <div class="container">
+          <div class="image">
+            <p>Dom's Favorite Hero The Milo King</p>
+            <img src="https://4.bp.blogspot.com/-BtuzkDDuVcU/WV9UpfARKNI/AAAAAAAAD0w/BbhY8uAL4KwGvIvDNO6uYrVGieFYVstLQCLcBGAs/s1600/milo%2Bman.jpg" alt="Milo King">
+          </div>
+          <div class="image">
+            <p>Charles Favorite Movie Tangled</p>
+            <img src="https://i.pinimg.com/originals/b3/0a/ff/b30aff959dec31c10892e2c3df286fc1.png" alt="Tangled">
+          </div>
+          <div class="image">
+            <p>Jared's Favorite Anime Character</p>
+            <img src="https://wallpapers.com/images/hd/katsuki-bakugo-my-hero-academia-iphone-hinkoepkef4nkqt2.jpg" alt="Jared's Bakugo">
+          </div>  
+          <div class="image">
+            <p>This is my friend Dom</p>
+            <img src="https://tse3.mm.bing.net/th/id/OIP.KpRPWqy3d4BwI3xbagZoBQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Dom">
+          </div>
+          <div class="image">
+            <p>This is my friend Charles</p>
+            <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Charles">
+          </div>
+          <div class="image">
+            <p>This is my friend Jared</p>
+            <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="jared">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
- name: 'PersonalProfile'
+  name: 'PersonalProfile'
 }
 </script>
+
 <style src="./css/personalprofile.css"></style>

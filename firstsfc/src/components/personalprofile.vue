76d609d942245014efbd15f8cea9fbc6686d47e8
Cristian Saldua <crsaldua@student.apc.edu.ph>
2026-01-20 09:19:42 +0800

    HSHSHSH

--- firstsfc/src/components/personalprofile.vue
+++ firstsfc/src/components/personalprofile.vue
@@ -2,69 +2,85 @@
   <div class="main-wrapper">
     <div class="grid-container">
       <div class="header">
-        <h1>Personal Profile Web page </h1>
-<p>
-    <b>
-Im Cristian<br>
-Asia Pacific College<br>
-Course: BSIT- 242 <br>
-IT Experience: html and css<br>
-Hobbies & Interest: Playing online game with my friends<br>
-Goals In Life / Dream:to be rich <br>
-</b>
+        <h1>Personal Profile Web page</h1>
+        <p>
+          <b>
+            Im Cristian<br>
+            Asia Pacific College<br>
+            Course: BSIT- 242<br>
+            IT Experience: html and css<br>
+            Hobbies & Interest: Playing```vue
+<template>
+  <div class="main-wrapper">
+    <div class="grid-container">
+      <div class="header">
+        <h1>Personal Profile Web page</h1>
+        <p>
+          <b>
+            Im Cristian<br>
+            Asia Pacific College<br>
+            Course: BSIT- 242<br>
+            IT Experience: html and css<br>
+            Hobbies & Interest: Playing online game with my friends<br>
+            Goals In Life / Dream: to be rich
+          </b>
+          <img 
+            src="https://media.licdn.com/dms/image/v2/D5603AQFpB9rCFiMUyw/profile-displayphoto-scale_200_200/B56ZqnKlsOKwAc-/0/1763741163274?e=2147483647&v=beta&t=vi0oROWu351SApec0r5CoHkB6NCjI2s3b4KE2pWIg-U" 
+            alt="Girl with a jacket" 
+            width="500" 
+            height="600"
+          >
+        </p>
+      </div>
 
-<img src="https://media.licdn.com/dms/image/v2/D5603AQFpB9rCFiMUyw/profile-displayphoto-scale_200_200/B56ZqnKlsOKwAc-/0/1763741163274?e=2147483647&v=beta&t=vi0oROWu351SApec0r5CoHkB6NCjI2s3b4KE2pWIg-U" alt="Girl with a jacket" width="500" height="600">
-</p>
+      <div class="header">
+        <h1>Responsive Image Gallery of jared and Charles</h1>
+        <p>Resize the browser window to see the responsive effect.</p>
+      </div>
 
-<div class="header">
-  <h1>Responsive Image Gallery of jared and Charles</h1>
-  <p>Resize the browser window to see the responsive effect.</p>
-</div>
+      <div class="container"> 
+        <div class="column">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+        </div>
+        
+        <div class="column">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+        </div>
 
+        <div class="column">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+        </div>
 
-
- <div class="container"> 
-  <div class="column">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+        <div class="column">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+          <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
+        </div>
+      </div>
+    </div>
   </div>
-  
-   <div class="column">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse4.mm.bing.net/th/id/OIP.gJ-ZwX3V3r2IXmVOP93PVgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-  </div>
-
-  <div class="column">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-  </div>
-  
-
-  <div class="column">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-    <img src="https://tse1.mm.bing.net/th/id/OIP.gbCwCpUr1rjCB6SxZuSs8AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" style="width:100%">
-  </div>
-  
 </template>
 
 <script>
